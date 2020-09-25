17feb615e6d030cdd501eaaa218811c6c23f09ec
hucc-git <56009393+hucc-git@users.noreply.github.com>
2020-09-25 11:34:17 +0800

    antd，less配置

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import { connect } from 'react-redux'
+import './test.less';
 import { Form, Input, Button, Checkbox } from 'antd';
 
 const layout = {
@@ -15,41 +15,43 @@ const tailLayout = {
 class App extends React.Component {
 
   render() {
-    console.log(this.props)
     return (
-      <Form
-        {...layout}
-        name="basic"
-        initialValues={{ remember: true }}
+      <div className="sty">
+        <Form
+          {...layout}
+          name="basic"
+          initialValues={{ remember: true }}
         // onFinish={onFinish}
         // onFinishFailed={onFinishFailed}
-      >
-        <Form.Item
-          label="Username"
-          name="username"
-          rules={[{ required: true, message: 'Please input your username!' }]}
         >
-          <Input />
-        </Form.Item>
+          <Form.Item
+            label="Username"
+            name="username"
+            rules={[{ required: true, message: 'Please input your username!' }]}
+          >
+            <Input />
+          </Form.Item>
 
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[{ required: true, message: 'Please input your password!' }]}
-        >
-          <Input.Password />
-        </Form.Item>
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: 'Please input your password!' }]}
+          >
+            <Input.Password />
+          </Form.Item>
 
-        <Form.Item {...tailLayout} name="remember" valuePropName="checked">
-          <Checkbox>Remember me</Checkbox>
-        </Form.Item>
+          <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
 
-        <Form.Item {...tailLayout}>
-          <Button type="primary" htmlType="submit">
-            Submit
+          <Form.Item {...tailLayout}>
+            <Button type="primary" htmlType="submit">
+              Submit
         </Button>
-        </Form.Item>
-      </Form>
+          </Form.Item>
+        </Form>
+        23
+      </div>
     );
   }
 
